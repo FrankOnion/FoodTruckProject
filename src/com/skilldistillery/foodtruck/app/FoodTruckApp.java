cfdabf621b3481e483f9b3967a1d93bcca2d335e
FrankOnion <lanaethonis@tuta.io>
2023-11-26 03:50:53 -0500

    User input and display

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,22 +1,49 @@
 package com.skilldistillery.foodtruck.app;
+import java.util.Scanner;
 
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
 
-	private FoodTruck[] fleet = new FoodTruck[5];
+    private FoodTruck[] fleet = new FoodTruck[5];
+    private int numTrucks = 0; // Track the number of trucks added
 
-	public static void main(String[] args) {
-		
-		FoodTruckApp app = new FoodTruckApp();
-		app.run();
-	}
-	
-	
-	public void run() {
-		
-		
-	}
-	
+    public static void main(String[] args) {
 
+        FoodTruckApp app = new FoodTruckApp();
+        app.run();
+    }
+
+    public void run() {
+        Scanner input = new Scanner(System.in);
+
+        // Input loop to gather food truck information
+        for (int i = 0; i < fleet.length; i++) {
+            System.out.print("Enter the name of the food truck (type 'quit' to exit): ");
+            String name = input.nextLine();
+
+            if (name.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            System.out.print("Enter the type of food served: ");
+            String foodType = input.nextLine();
+
+            System.out.print("Enter the rating (1-5): ");
+            double rating = input.nextDouble();
+            input.nextLine(); // Consume the newline character
+
+            // Create a new FoodTruck object and add it to the array
+            fleet[i] = new FoodTruck(name, foodType, rating);
+            numTrucks++;
+        }
+
+        // Display the list of entered food trucks
+        System.out.println("\nList of Entered Food Trucks:");
+        for (int i = 0; i < numTrucks; i++) {
+            System.out.println(fleet[i]);
+        }
+
+        input.close();
+    }
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,4 +2,46 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 
+    // Fields
+    private String name;
+    private String foodType;
+    private double rating;
+
+    // Constructors
+    public FoodTruck(String name, String foodType, double rating) {
+        this.name = name;
+        this.foodType = foodType;
+        this.rating = rating;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    // Override toString method to display truck details
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nFood Type: " + foodType + "\nRating: " + rating;
+    }
 }
